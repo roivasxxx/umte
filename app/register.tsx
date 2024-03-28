0498a78354ffde2ab6749acdf144cbf33719356e
roivasxxx <tiragoshi@gmail.com>
2024-03-28 16:55:11 +0100

    remove reanimated package

--- app/register.tsx
+++ app/register.tsx
@@ -3,25 +3,10 @@ import LoginForm from "@/components/auth/loginForm"
 import { Colors } from "@/constants/Colors"
 import styles from "@/constants/styles"
 import React from "react"
-import {
-    ActivityIndicator,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    View,
-} from "react-native"
+import { ActivityIndicator, View } from "react-native"
 
 export default function Register() {
-    const { signUp, loading } = useSession()
-    const [userState, setUserState] = React.useState({
-        email: "test@test.cz",
-        password: "password",
-        error: null,
-    })
-
-    const setError = (e: any) => {
-        setUserState({ ...userState, error: e })
-    }
+    const { loading } = useSession()
 
     if (loading) {
         return (
